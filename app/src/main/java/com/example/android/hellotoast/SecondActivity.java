6d55a776d9b0fdf77e0655c9b7a4a2c569ab1c48
wongky87 <wongky-wm19@student.tarc.edu.my>
2020-08-04 01:18:50 +0800

    Practical_6_HelloToast

---
+++ app/src/main/java/com/example/android/hellotoast/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String count = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView setCount = findViewById(R.id.count);
+        setCount.setText(count);
+    }
+}
